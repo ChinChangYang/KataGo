53db0047965af20c14c0ddae0e5b46e761c179a1
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2024-08-20 18:38:27 +0800

    Refactor script to use relative paths
    Updated the ci_post_clone.sh script to replace absolute paths with relative paths for resource file operations. This improves portability and reduces dependency on the CI_PRIMARY_REPOSITORY_PATH variable.

--- ios/KataGo iOS/ci_scripts/ci_post_clone.sh
+++ ios/KataGo iOS/ci_scripts/ci_post_clone.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 curl -L -o default_model.bin.gz https://github.com/ChinChangYang/KataGo/releases/download/v1.15.1-coreml2/kata1-b18c384nbt-s9996604416-d4316597426.bin.gz
-cp -f default_model.bin.gz "${CI_PRIMARY_REPOSITORY_PATH}/ios/KataGo\ iOS/Resources/default_model.bin.gz"
+cp -f default_model.bin.gz ../Resources/default_model.bin.gz
 
 curl -L -o b18c384nbt-humanv0.bin.gz https://github.com/lightvector/KataGo/releases/download/v1.15.0/b18c384nbt-humanv0.bin.gz
-cp -f b18c384nbt-humanv0.bin.gz "${CI_PRIMARY_REPOSITORY_PATH}/ios/KataGo\ iOS/Resources/b18c384nbt-humanv0.bin.gz"
+cp -f b18c384nbt-humanv0.bin.gz ../Resources/b18c384nbt-humanv0.bin.gz
 
-if [ -f "${CI_PRIMARY_REPOSITORY_PATH}/ios/KataGo\ iOS/Resources/KataGoModel29x29fp16.mlpackage" ]; then
+if [ -f ../Resources/KataGoModel29x29fp16.mlpackage ]; then
     rm -f KataGoModel29x29fp16v14s9996604416.mlpackage.zip
     curl -L -o KataGoModel29x29fp16v14s9996604416.mlpackage.zip https://github.com/ChinChangYang/KataGo/releases/download/v1.15.1-coreml2/KataGoModel29x29fp16v14s9996604416.mlpackage.zip
     unzip KataGoModel29x29fp16v14s9996604416.mlpackage.zip
-    mv KataGoModel29x29fp16v14s9996604416.mlpackage "${CI_PRIMARY_REPOSITORY_PATH}/ios/KataGo\ iOS/Resources/KataGoModel29x29fp16.mlpackage"
+    mv KataGoModel29x29fp16v14s9996604416.mlpackage ../Resources/KataGoModel29x29fp16.mlpackage
 fi
 
-if [ -f "${CI_PRIMARY_REPOSITORY_PATH}/ios/KataGo\ iOS/Resources/KataGoModel29x29fp16m1.mlpackage" ]; then
+if [ -f ../Resources/KataGoModel29x29fp16m1.mlpackage ]; then
     rm -f KataGoModel29x29fp16v15m1humanv0.mlpackage.zip
     curl -L -o KataGoModel29x29fp16v15m1humanv0.mlpackage.zip https://github.com/ChinChangYang/KataGo/releases/download/v1.15.1-coreml2/KataGoModel29x29fp16v15m1humanv0.mlpackage.zip
     unzip KataGoModel29x29fp16v15m1humanv0.mlpackage.zip
-    mv KataGoModel29x29fp16v15m1humanv0.mlpackage "${CI_PRIMARY_REPOSITORY_PATH}/ios/KataGo\ iOS/Resources/KataGoModel29x29fp16m1.mlpackage"
+    mv KataGoModel29x29fp16v15m1humanv0.mlpackage ../Resources/KataGoModel29x29fp16m1.mlpackage
 fi
