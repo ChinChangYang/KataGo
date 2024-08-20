a6f94af4a3bd59e5a7729d1aded512c21884a3bb
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2024-08-20 19:04:26 +0800

    Update logic in ci_post_clone.sh to correctly download models
    Changed the conditions to check for the existence of the .mlpackage files, ensuring that the models are downloaded only if they do not exist.

--- ios/KataGo iOS/ci_scripts/ci_post_clone.sh
+++ ios/KataGo iOS/ci_scripts/ci_post_clone.sh
@@ -5,14 +5,14 @@ cp -f default_model.bin.gz ../Resources/default_model.bin.gz
 curl -L -o b18c384nbt-humanv0.bin.gz https://github.com/lightvector/KataGo/releases/download/v1.15.0/b18c384nbt-humanv0.bin.gz
 cp -f b18c384nbt-humanv0.bin.gz ../Resources/b18c384nbt-humanv0.bin.gz
 
-if [ -f ../Resources/KataGoModel29x29fp16.mlpackage ]; then
+if [ ! -f ../Resources/KataGoModel29x29fp16.mlpackage ]; then
     rm -f KataGoModel29x29fp16v14s9996604416.mlpackage.zip
     curl -L -o KataGoModel29x29fp16v14s9996604416.mlpackage.zip https://github.com/ChinChangYang/KataGo/releases/download/v1.15.1-coreml2/KataGoModel29x29fp16v14s9996604416.mlpackage.zip
     unzip KataGoModel29x29fp16v14s9996604416.mlpackage.zip
     mv KataGoModel29x29fp16v14s9996604416.mlpackage ../Resources/KataGoModel29x29fp16.mlpackage
 fi
 
-if [ -f ../Resources/KataGoModel29x29fp16m1.mlpackage ]; then
+if [ ! -f ../Resources/KataGoModel29x29fp16m1.mlpackage ]; then
     rm -f KataGoModel29x29fp16v15m1humanv0.mlpackage.zip
     curl -L -o KataGoModel29x29fp16v15m1humanv0.mlpackage.zip https://github.com/ChinChangYang/KataGo/releases/download/v1.15.1-coreml2/KataGoModel29x29fp16v15m1humanv0.mlpackage.zip
     unzip KataGoModel29x29fp16v15m1humanv0.mlpackage.zip
