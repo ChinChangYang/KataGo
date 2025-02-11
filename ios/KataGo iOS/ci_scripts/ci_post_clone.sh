dc5930bffa0825660f6296ed29883d3b8c7c077f
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2025-02-11 22:25:14 +0800

    Update ci_post_clone.sh for Core ML model
    Modified the script to download the KataGoModel29x29fp16w8LiCh-s8032M.mlpackage, which is the currently strongest confidently-rated network. Removed the previous model's conditional checks and implemented download and cleanup processes. This ensures that the latest and most powerful model is used, enhancing the performance and capabilities of the application.

--- ios/KataGo iOS/ci_scripts/ci_post_clone.sh
+++ ios/KataGo iOS/ci_scripts/ci_post_clone.sh
@@ -5,12 +5,11 @@ cp -f default_model.bin.gz ../Resources/default_model.bin.gz
 curl -L -o b18c384nbt-humanv0.bin.gz https://github.com/ChinChangYang/KataGo/releases/download/v1.15.3-coreml1/b18c384nbt-humanv0-null.bin.gz
 cp -f b18c384nbt-humanv0.bin.gz ../Resources/b18c384nbt-humanv0.bin.gz
 
-if [ ! -f ../Resources/KataGoModel29x29fp16.mlpackage ]; then
-    rm -f KataGoModel29x29fp16w8LiCh.mlpackage.zip
-    curl -L -o KataGoModel29x29fp16w8LiCh.mlpackage.zip https://github.com/ChinChangYang/KataGo/releases/download/v1.15.3-coreml1/KataGoModel29x29fp16w8LiCh.mlpackage.zip
-    unzip KataGoModel29x29fp16w8LiCh.mlpackage.zip
-    mv KataGoModel29x29fp16w8LiCh.mlpackage ../Resources/KataGoModel29x29fp16.mlpackage
-fi
+rm -rf ../Resources/KataGoModel29x29fp16.mlpackage
+rm -f KataGoModel29x29fp16w8LiCh-s8032M.mlpackage.zip
+curl -L -o KataGoModel29x29fp16w8LiCh-s8032M.mlpackage.zip https://github.com/ChinChangYang/KataGo/releases/download/v1.15.3-coreml1/KataGoModel29x29fp16w8LiCh-s8032M.mlpackage.zip
+unzip KataGoModel29x29fp16w8LiCh-s8032M.mlpackage.zip
+mv KataGoModel29x29fp16w8LiCh-s8032M.mlpackage ../Resources/KataGoModel29x29fp16.mlpackage
 
 if [ ! -f ../Resources/KataGoModel29x29fp16m1.mlpackage ]; then
     rm -f KataGoModel29x29fp16m1w8LiCh.mlpackage.zip
