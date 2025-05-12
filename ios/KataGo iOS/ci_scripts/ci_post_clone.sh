f79322ee3b5346fbf7c70935104226eb85dc46e5
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2025-05-12 13:20:12 +0800

    Update download links in ci_post_clone.sh to use latest model

--- ios/KataGo iOS/ci_scripts/ci_post_clone.sh
+++ ios/KataGo iOS/ci_scripts/ci_post_clone.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 rm -f default_model.bin.gz
-curl -L -o default_model.bin.gz https://github.com/ChinChangYang/KataGo/releases/download/v1.15.3-coreml1/b28c512nbt-null-s8032M.bin.gz
+curl -L -o default_model.bin.gz https://github.com/ChinChangYang/KataGo/releases/download/v1.15.3-coreml1/b28c512nbt-null-s8834M.bin.gz
 cp -f default_model.bin.gz ../Resources/default_model.bin.gz
 
 curl -L -o b18c384nbt-humanv0.bin.gz https://github.com/ChinChangYang/KataGo/releases/download/v1.15.3-coreml1/b18c384nbt-humanv0-null.bin.gz
 cp -f b18c384nbt-humanv0.bin.gz ../Resources/b18c384nbt-humanv0.bin.gz
 
 rm -rf ../Resources/KataGoModel29x29fp16.mlpackage
-rm -f KataGoModel29x29fp16w8LiCh-s8032M.mlpackage.zip
-curl -L -o KataGoModel29x29fp16w8LiCh-s8032M.mlpackage.zip https://github.com/ChinChangYang/KataGo/releases/download/v1.15.3-coreml1/KataGoModel29x29fp16w8LiCh-s8032M.mlpackage.zip
-unzip KataGoModel29x29fp16w8LiCh-s8032M.mlpackage.zip
-mv KataGoModel29x29fp16w8LiCh-s8032M.mlpackage ../Resources/KataGoModel29x29fp16.mlpackage
+rm -f KataGoModel29x29fp16w8LiCh-s8834M.mlpackage.zip
+curl -L -o KataGoModel29x29fp16w8LiCh-s8834M.mlpackage.zip https://github.com/ChinChangYang/KataGo/releases/download/v1.15.3-coreml1/KataGoModel29x29fp16w8LiCh-s8834M.mlpackage.zip
+unzip KataGoModel29x29fp16w8LiCh-s8834M.mlpackage.zip
+mv KataGoModel29x29fp16w8LiCh-s8834M.mlpackage ../Resources/KataGoModel29x29fp16.mlpackage
 
 if [ ! -f ../Resources/KataGoModel29x29fp16m1.mlpackage ]; then
     rm -f KataGoModel29x29fp16m1w8LiCh.mlpackage.zip
